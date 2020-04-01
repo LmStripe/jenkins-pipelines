5e438d41b8ce7f3649c5678f45adf53c019d2f07
Laziz Mansurov <lazizmansurov4@gmail.com>
2020-03-31 19:55:03 -0500

    added line

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -2,6 +2,7 @@ node {
 	properties([
 		// Below line sets "Discard Builds more than 5"
 		buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
+		
 		// Below line triggers this job every minute
 		pipelineTriggers([cron('* * * * *')])
 		])
