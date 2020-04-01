44cba07f9b49627f425aad0f6cc2c1f585445f32
Laziz Mansurov <lazizmansurov4@gmail.com>
2020-03-31 20:36:04 -0500

    updated

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -6,14 +6,28 @@ node {
 		// Below line triggers this job every minute
 		pipelineTriggers([pollSCM('* * * * *')])
 		])
+        // Below line triggers this job every minute 
+		pipelineTriggers([pollSCM('* * * * *')]),
+		parameters([choice(choices: [
+			'dev1.lazizm.com', 
+			'qa1.lazizm.com', 
+			'stage1.lazizm.com', 
+			'prod1.lazizm.com'], 
+			description: 'Please choose an environment', 
+			name: 'ENVIR')]), 
+		])
+
+    // Pulls repo from developer
 	stage("Pull Repo"){
 		git   'https://github.com/farrukh90/cool_website.git'
 	}
+
 	stage("Install Prerequisites"){
 		sh """
 		ssh centos@jenkins_worker1.lazizm.com                 sudo yum install httpd -y
 		"""
 	}
+	   //Copies over developers files to different enviroment
 	stage("Copy artifacts"){
 		sh """
 		scp -r *  centos@jenkins_worker1.lazizm.com:/tmp
@@ -23,9 +37,12 @@ node {
 		ssh centos@jenkins_worker1.lazizm.com				   sudo chmod 777 /var/www/html/*
 		"""
 	}
+	    //Restart web server
 	stage("Restart web server"){
 		sh "ssh centos@jenkins_worker1.lazizm.com                 sudo systemctl restart httpd"
 	}
+
+	//Sends a message to slack 
 	stage("Slack"){
 		slackSend color: '#BADA55', message: 'Hello, World!'
 	}
