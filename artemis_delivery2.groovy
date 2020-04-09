a50e684f27571dad2e7d81d624dba1f2d62c97f5
Laziz Mansurov <lazizmansurov4@gmail.com>
2020-04-09 18:37:32 -0500

    updated delivery 2

--- artemis_delivery2.groovy
+++ artemis_delivery2.groovy
@@ -1,19 +1,13 @@
 node {
-	properties(
-		[parameters([
-            choice(choices: 
-				[
+properties(
+	[parameters(
+		[choice(choices: 
+			[
 				'0.1', 
 				'0.2', 
 				'0.3', 
 				'0.4', 
-				'0.5',
-				'0.6',
-				'0.7',
-				'0.8',
-				'0.9',
-				'10',
-			], 
+				'0.5'], 
 		description: 'Which version of the app should I deploy? ', 
 		name: 'Version'), 
 	choice(choices: 
@@ -31,6 +25,10 @@ node {
 
 
 
+
+
+
+
 		stage("Stage1"){
 			timestamps {
 				ws {
@@ -38,20 +36,20 @@ node {
 			}
 		}
 		stage("Get Credentials"){
-		timestamps {
-			ws{
-				sh '''
-					aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 713287746880.dkr.ecr.us-east-1.amazonaws.com/artemis
-					'''
+			timestamps {
+				ws{
+					sh '''
+						aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 713287746880.dkr.ecr.us-east-1.amazonaws.com/artemis
+						'''
 				}
 			}
 		}
 		stage("Build Docker Image"){
-		timestamps {
-			ws {
-				sh '''
-					docker build -t artemis:${Version} .
-					'''
+			timestamps {
+				ws {
+					sh '''
+						docker build -t artemis:${Version} .
+						'''
 				}
 			}
 		}
@@ -60,10 +58,10 @@ node {
 				ws {
 					sh '''
 						docker tag artemis:${Version} 713287746880.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
-					'''
-					}
+						'''
 				}
 			}
+		}
 		stage("Push Image"){
 			timestamps {
 				ws {
@@ -78,28 +76,20 @@ node {
 				ws {
 					echo "Slack"
 					//slackSend color: '#BADA55', message: 'Hello, World!'
-				}
-			}
-		}
-		stage("Authenticate"){
-			timestamps {
-				ws {
-					sh '''
-						ssh centos@${ENVIR} $(aws ecr get-login --no-include-email --region us-east-1)
-						'''
-				}
-			}
-		}
-		stage("Clean Up"){
+            }
+        }
+    
+    }
+    		stage("Clean Up"){
 			timestamps {
 				ws {
 					try {
 						sh '''
 							#!/bin/bash
-							IMAGES=$(ssh centos@${ENVIR} docker ps -aq) 
+							IMAGES=$(ssh centos@dev1.acirrustech.com docker ps -aq) 
 							for i in \$IMAGES; do
-								ssh centos@${ENVIR} docker stop \$i
-								ssh centos@${ENVIR} docker rm \$i
+								ssh centos@dev1.acirrustech.com docker stop \$i
+								ssh centos@dev1.acirrustech.com docker rm \$i
 							done 
 							'''
 					} catch(e) {
@@ -108,14 +98,14 @@ node {
 					}
 				}
 			}
-		
-	stage("Run Container"){
+
+    	stage("Run Container"){
 		timestamps {
 			ws {
 				sh '''
-					ssh centos@${ENVIR} docker run -dti -p 5001:5000 713287746880.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
+					ssh centos@dev1.acirrustech.com docker run -dti -p 5001:5000 713287746880.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
 					'''
-				}
-			}
-		}
+            }
+        }
+    }
 }
